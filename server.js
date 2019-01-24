e01d5d974a508b2320ac37bc50338855cd4066bd
Shardayel17 <Shardayel17@yahoo.com>
2019-01-24 14:44:48 -0500

    nodemailer working

--- server.js
+++ server.js
@@ -6,31 +6,32 @@ const mongoose = require("mongoose");
 const app = express();
 const guestRoutes = require("./routes/api/guest");
 const eventRoutes = require('./routes/api/event');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'youremail@gmail.com',
-//     pass: 'yourpassword'
-//   }
-// });
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'TheMSJExperience@gmail.com',
+    pass: 'Msjlps2019'
+  }
+});
 
-// var mailOptions = {
-//   from: 'TheMSJExperience@gmail.com',
-//   to: 'shardayel17@yahoo.com',
-//   subject: 'Sending Email using Node.js',
-//   text: 'That was easy!'
-// };
+var mailOptions = {
+  from: 'TheMSJExperience@gmail.com',
+  to: 'shardayel17@yahoo.com',
+  subject: 'You have been invited to attend a weedding for:',
+  text: 'Stacy and Mike'
 
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// }); 
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
 
 
 // // nodemailer stuff to send emails to guests
